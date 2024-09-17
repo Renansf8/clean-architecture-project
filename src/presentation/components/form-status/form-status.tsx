1588ff9142fbb7ad9c57d03e415b2e8e62a8a1ed
Renan Santiago <renansferreira8@gmail.com>
2024-09-16 23:25:17 -0300

    feat: ensure Login start with input with required state

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,11 +4,11 @@ import Spinner from "../spinner/spinner"
 import Context from '../../contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { isLoading, errorMessage } = useContext(Context)
+  const { state, errorState } = useContext(Context)
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
-      {isLoading && <Spinner className={Styles.spinner} />}
-      {errorMessage && <p className={Styles.error}>{errorMessage}</p>}
+      {state.isLoading && <Spinner className={Styles.spinner} />}
+      {errorState.main && <p className={Styles.error}>{errorState.main}</p>}
     </div>
   )
 }
--- src/presentation/contexts/form/form-context.ts
+++ src/presentation/contexts/form/form-context.ts
@@ -1,8 +1,23 @@
 import { createContext } from 'react'
 
 type FormConntextProps = {
-  isLoading: boolean,
-  errorMessage: string
+  state: {
+    isLoading: boolean, 
+  },
+  errorState: {
+    email: string,
+    password: string,
+    main: string,
+  },
 }
 
-export default createContext<FormConntextProps>({ isLoading: false, errorMessage: '' })
+export default createContext<FormConntextProps>({
+  state: {
+    isLoading: false, 
+  },
+  errorState: {
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
+  },
+})
