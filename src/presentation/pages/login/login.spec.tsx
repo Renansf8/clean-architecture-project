909251242de03c936a85527fb2b35302db62912b
Renan Santiago <renansferreira8@gmail.com>
2024-09-24 11:14:19 -0300

    tesst: ensure Login present error if email is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -10,6 +10,7 @@ type SutTypes = {
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy()
+  validationSpy.errorMessage = 'mensagem de erro'
   const sut = render(<Login validation={validationSpy} />)
   return {
     sut,
@@ -20,16 +21,16 @@ const makeSut = (): SutTypes => {
 describe('Login Component', () => {
   afterEach(cleanup)
   test('Should start with initial state', () => {
-    const { sut } = makeSut()
+    const { sut, validationSpy } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
     const emailStatus = sut.getByTestId('email-status')
-    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.title).toBe(validationSpy.errorMessage)
     expect(emailStatus.textContent).toBe('🛑')
     const passwordStatus = sut.getByTestId('password-status')
-    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.title).toBe(validationSpy.errorMessage)
     expect(emailStatus.textContent).toBe('🛑')
   })
 
@@ -49,5 +50,15 @@ describe('Login Component', () => {
     fireEvent.input(passwordInput, { target: { value: 'any_password' } })
     expect(validationSpy.fieldName).toBe('password')
     expect(validationSpy.fieldValue).toBe('any_password')
+  })
+
+  test('Should show email error if Validation fails', () => {
+    const { sut, validationSpy } = makeSut()
+
+    const emailInput = sut.getByTestId("email")
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe(validationSpy.errorMessage)
+    expect(emailStatus.textContent).toBe('🛑')
   })
 })
