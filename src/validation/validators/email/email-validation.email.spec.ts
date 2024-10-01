d84f94ebd27f6081b3e7aaf10c1218507032a4a6
Renan Santiago <renansferreira8@gmail.com>
2024-10-01 11:26:29 -0300

    test: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/email/email-validation.email.spec.ts
+++ src/validation/validators/email/email-validation.email.spec.ts
@@ -3,7 +3,13 @@ import { EmailValidation } from "./email-validation"
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
     const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const error = sut.validate('qualquer_palavra')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation('email')
+    const error = sut.validate('email@teste.com')
+    expect(error).toBeFalsy()
   })
 })
